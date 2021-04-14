a61a64e6894d07bf911d1a657f033722fc6bf84a
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-04-14 14:31:06 -0400

    Continued adding variables to the orb

--- src/scripts/docker-tags.sh
+++ src/scripts/docker-tags.sh
@@ -1,5 +1,64 @@
 DockerTags() {
-    echo  Checking version: "${PARAM_VERSION}" against image: "${PARAM_ORG}"/"${PARAM_REPO}"
+    echo  Checking version: "${PARAM_VERSION}" against repository: "${PARAM_ORG}"/"${PARAM_REPO}"
+
+    NEW_VERSION_MAJOR=$(echo ${NEW_VERSION} | awk -F . '{print $1}' )
+    NEW_VERSION_MINOR=$(echo ${NEW_VERSION} | awk -F . '{print $2}' )
+    NEW_VERSION_HF=$(echo ${NEW_VERSION} | awk -F . '{print $3}' )
+    NEW_VERSION_CLASSIFIER=$(echo ${NEW_VERSION} | awk -F - '{print $2}' )
+
+    if [ "$NEW_VERSION_CLASSIFIER" != "" ]; then
+        NEW_VERSION_CLASSIFIER='-'$NEW_VERSION_CLASSIFIER
+    fi
+
+    AUTH_DOMAIN="auth.docker.io"
+    AUTH_SERVICE="registry.docker.io"
+    AUTH_SCOPE="repository:${DOCKER_HUB_ORG}/${DOCKER_HUB_REPO}:pull"
+    AUTH_OFFLINE_TOKEN="1"
+    AUTH_CLIENT_ID="shell"
+
+    API_DOMAIN="registry-1.docker.io"
+
+    TOKEN=$(curl -s -X GET -u ${DOCKER_HUB_USER}:${DOCKER_HUB_PASSWORD} "https://${AUTH_DOMAIN}/token?service=${AUTH_SERVICE}&scope=${AUTH_SCOPE}&offline_token=${AUTH_OFFLINE_TOKEN}&client_id=${AUTH_CLIENT_ID}" | jq -r '.token')
+    VERSIONS=$(curl -s -H "Authorization: Bearer ${TOKEN}" https://${API_DOMAIN}/v2/${DOCKER_HUB_ORG}/${DOCKER_HUB_REPO}/tags/list | jq -r '.tags[]' | grep -E "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$NEW_VERSION_CLASSIFIER$")
+
+    if ! (echo "$VERSIONS" | grep $NEW_VERSION); then
+        echo $NEW_VERSION not found yet, adding it
+        VERSIONS=$(echo -e "${VERSIONS}\n${NEW_VERSION}")
+    fi
+
+    VERSIONS=$(echo "$VERSIONS" | sort --version-sort)
+
+    echo All versions : $VERSIONS
+
+    LATEST=$(echo "$VERSIONS" | tail -1)
+    MATCHING1=$(echo "$VERSIONS" | grep -E "^${NEW_VERSION_MAJOR}\." | tail -1)
+    MATCHING2=$(echo "$VERSIONS" | grep -E "^${NEW_VERSION_MAJOR}\.${NEW_VERSION_MINOR}\." | tail -1)
+    MATCHING3=$(echo "$VERSIONS" | grep -E "^${NEW_VERSION_MAJOR}\.${NEW_VERSION_MINOR}\.${NEW_VERSION_HF}\." | tail -1)
+
+
+    if [ "$LATEST" == "$NEW_VERSION" ]; then
+        echo latest${NEW_VERSION_CLASSIFIER} is : $LATEST , require tag update
+    else
+        echo latest${NEW_VERSION_CLASSIFIER} is : $LATEST , unchanged
+    fi
+
+    if [ "$MATCHING1" == "$NEW_VERSION" ]; then
+        echo ${NEW_VERSION_MAJOR}${NEW_VERSION_CLASSIFIER} is : $MATCHING1 , require tag update
+    else
+        echo ${NEW_VERSION_MAJOR}${NEW_VERSION_CLASSIFIER} is : $MATCHING1 , unchanged
+    fi
+
+    if [ "$MATCHING2" == "$NEW_VERSION" ]; then
+        echo ${NEW_VERSION_MAJOR}.${NEW_VERSION_MINOR}${NEW_VERSION_CLASSIFIER} is : $MATCHING2 , require tag update
+    else
+        echo ${NEW_VERSION_MAJOR}.${NEW_VERSION_MINOR}${NEW_VERSION_CLASSIFIER} is : $MATCHING2 , unchanged
+    fi
+
+    if [ "$MATCHING3" == "$NEW_VERSION" ]; then
+        echo ${NEW_VERSION_MAJOR}.${NEW_VERSION_MINOR}.${NEW_VERSION_HF}${NEW_VERSION_CLASSIFIER} is : $MATCHING3 , require tag update
+    else
+        echo ${NEW_VERSION_MAJOR}.${NEW_VERSION_MINOR}.${NEW_VERSION_HF}${NEW_VERSION_CLASSIFIER} is : $MATCHING3 , unchanged
+    fi    
 }
 
 # Will not run if sourced for bats-core tests.
--- src/tests/docker-tags.bats
+++ src/tests/docker-tags.bats
@@ -7,7 +7,9 @@ setup() {
 @test '1: Run DockerTags' {
     # Mock environment variables or functions by exporting them (after the script has been sourced)
     export PARAM_VERSION="1.0.0.0"
+    export PARAM_VERSION="jahia"
+    export PARAM_VERSION="jahia"
     # Capture the output of our "DockerTags" function
     result=$(DockerTags)
-    [ "$result" == "Received version: 1.0.0.0" ]
+    [ "$result[0]" == "Checking version: 1.0.0.0 against repository: jahia/jahia" ]
 }
